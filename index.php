670685b86573cf16d1d0cabbbbc3132cf20a0bab
miakojason <miakojason@gmail.com>
2023-10-02 13:42:03 +0800

    判斷式練習+switch css應用

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <?php
+  define('age',30);
+  $age=30;
+
+  $age=50;
+  echo $age;
+  echo age;
+  $name='劉勤永';
+  echo $name;
+  
+  $a=15;
+  $b=45;
+  $c=$a;
+  $a=$b;
+  $b=$c;
+  echo $a;
+  echo $b;
+  ?>
+ 
+</body>
+
+</html>
